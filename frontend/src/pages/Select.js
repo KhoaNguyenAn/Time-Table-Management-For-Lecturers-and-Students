dd8f60cbcdbb0adf1f2a867ab0bcd5a6c16b25a9
KhoaNguyenAn <nguyenankhoa1402@gmail.com>
2023-11-30 00:50:06 +1100

    abc

--- frontend/src/pages/Select.js
+++ frontend/src/pages/Select.js
@@ -3,7 +3,7 @@ const Select = () => {
         <div class="overflow-auto h-72 relative max-w-sm mx-auto bg-white dark:bg-slate-800 dark:highlight-white/5 shadow-lg ring-1 ring-black/5 rounded-xl flex flex-col divide-y dark:divide-slate-200/5">
             <div class="flex items-center gap-4 p-4">
                 <div class="flex flex-col">
-                    <strong class="text-slate-900 text-sm font-medium dark:text-slate-200">Andrew Alfred</strong>
+                    <strong class="text-slate-900 text-sm font-medium dark:text-slate-200">Andrew Alfred abc</strong>
                     <span class="text-slate-500 text-sm font-medium dark:text-slate-400">Technical advisor</span>
                 </div>
             </div>
