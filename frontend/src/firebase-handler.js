cff3e3440cf0f34253d3ffe7d7951f76d15cfdd6
sguh0003 <sguh0003@student.monash.edu>
2023-12-02 20:04:08 +0800

    Fixed initialisation of firestore and added test file.

--- frontend/src/firebase-handler.js
+++ frontend/src/firebase-handler.js
@@ -15,6 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 export const auth = getAuth(app);
-export default getFirestore();
+export const db = getFirestore(app);
--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,16 +1,29 @@
-import db from "../firebase-handler"
+import React, {useRef, useState} from "react";
+import {db} from "../firebase-handler";
+import { addDoc, collection } from "firebase/firestore";
 
-const Test = () => {
-    db.collection("users").add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-    });
-};
-export default Test
+export default function Test() {
+  const nameRef = useRef();
+  const ref = collection(db, "Names");
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+
+    let data = {
+      message: nameRef.current.value,
+    };
+
+    addDoc(ref, data);
+  };
+  
+  return (
+    <div>
+      <form onSubmit={handleSave}>
+        <label>Enter a Name</label>
+        <input type="text" ref={nameRef} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  )
+}
